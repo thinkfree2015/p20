074d72d619cf14b996ed15b6f7deb6d26a863c55
kaysonyang <kayson_yang@163.com>
2015-12-10 14:41:36 +0800

    项目基础

--- ef-courier/src/main/java/com/efeiyi/ec/courier/grasp/service/impl/ThreadManagerImpl.java
+++ ef-courier/src/main/java/com/efeiyi/ec/courier/grasp/service/impl/ThreadManagerImpl.java
@@ -20,10 +20,19 @@ public class ThreadManagerImpl implements ThreadManager {
     public void startWork() throws Exception{
         log.info("开始创建线程池");
         ExecutorService pool = Executors.newFixedThreadPool(OrganizationConst.THREAD_POOL_CORE_COUNT);
-        int beginNum =1,endNum =1;
+        int beginNum =1,endNum =40;
         for(int i = 1; i <= OrganizationConst.THREAD_POOL_CORE_COUNT; i++) {
-            pool.execute(new Thread(new Helper(i,i*40)));
+            pool.execute(new Thread(new Helper(beginNum,endNum)));
+            beginNum = endNum+1;
+            if (i==OrganizationConst.THREAD_POOL_CORE_COUNT-1){
+                endNum=373;
+            }else {
+                endNum = beginNum*40;
+            }
+
         }
+        log.info("创建线程池完成");
         pool.shutdown();
+        log.info("关闭线程池");
     }
 }
