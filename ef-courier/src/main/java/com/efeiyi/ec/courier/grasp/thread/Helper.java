2247704d346c75803f66486cf6ae8fc4be1d710a
efeiyi007 <zkguigui@foxmail.com>
2015-12-11 16:20:09 +0800

    项目基础

--- ef-courier/src/main/java/com/efeiyi/ec/courier/grasp/thread/Helper.java
+++ ef-courier/src/main/java/com/efeiyi/ec/courier/grasp/thread/Helper.java
@@ -1,11 +1,16 @@
 package com.efeiyi.ec.courier.grasp.thread;
 
 
+import com.efeiyi.ec.courier.grasp.service.impl.ContentDataManagerImpl;
+import com.efeiyi.ec.courier.organization.util.ContextUtils;
+import org.apache.log4j.Logger;
+
 /**
  * Created by Administrator on 2015/12/10.
  *
  */
 public class Helper implements  Runnable {
+    private static Logger log = Logger.getLogger(Helper.class);
     private int beginNum;
     private int endNum;
 
@@ -33,5 +38,16 @@ public class Helper implements  Runnable {
     @Override
     public void run() {
      System.out.println("beginNum:"+beginNum+"   "+"endNum:"+endNum);
+        exeBatchInsert();
+    }
+
+    private void exeBatchInsert(){
+        log.info("begin exeBatchInsert:"+beginNum+"-->"+endNum);
+        try{
+            ((ContentDataManagerImpl)ContextUtils.getBean("contentDataManagerImpl")).findCityList(beginNum,endNum);
+        }catch(Exception e){
+            log.error(e);
+        }
+        log.info("end exeBatchInsert:"+beginNum+"-->"+endNum);
     }
 }
