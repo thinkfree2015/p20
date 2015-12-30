aa4bfe24d36f87486b6153fb6e2f994247531d42
lylylyly <360682644@qq.com>
2015-12-30 17:59:33 +0800

    答题Service

--- jh-system/src/main/java/com/efeiyi/ec/system/yale/question/service/WxLoginManager.java
+++ jh-system/src/main/java/com/efeiyi/ec/system/yale/question/service/WxLoginManager.java
@@ -2,7 +2,6 @@ package com.efeiyi.ec.system.yale.question.service;
 
 import com.efeiyi.ec.organization.model.Consumer;
 
-
 /**
  * Created by Administrator on 2015/12/30.
  */
@@ -10,5 +9,5 @@ public interface WxLoginManager {
 
     String [] getAccessToken(String code) throws Exception;
 
-    Consumer saveOrGetConsumer(String accessToken,String openId) throws Exception;
+    Consumer saveOrGetConsumer(String accessToken, String openId) throws Exception;
 }
