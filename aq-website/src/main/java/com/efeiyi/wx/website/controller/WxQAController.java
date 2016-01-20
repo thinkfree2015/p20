5f342eccec33f144dff746b9be8ceb63e7181d44
lylylyly <360682644@qq.com>
2016-01-20 12:51:54 +0800

    答题不用的controller注释掉竟然会No Spring WebApplicationInitializer types detected on classpath

--- aq-website/src/main/java/com/efeiyi/wx/website/controller/WxQAController.java
+++ aq-website/src/main/java/com/efeiyi/wx/website/controller/WxQAController.java
@@ -21,57 +21,57 @@ import java.util.List;
 @RequestMapping("/wx")
 public class WxQAController {
 
-//    @Autowired
-//    private BaseManager baseManager;
-//
-//    @RequestMapping("/start.do")
-//    public ModelAndView start(){
-//        return new ModelAndView("/question/activityDescription");
-//    }
-//
-//    @RequestMapping("/startHelp.do")
-//    public ModelAndView startHelp(HttpServletRequest request, ModelMap modelMap)throws Exception{
-//        String examId = request.getParameter("examId");
-//        Examination exam = (Examination) baseManager.getObject(Examination.class.getName(), examId);
-//
-//        modelMap.put("examination", exam);
-//
-//        return new ModelAndView("/question/activityDescription");
-//    }
-//
-//    @RequestMapping("/shareReturn.do")
-//    public ModelAndView shareReturn(HttpServletRequest request, ModelMap modelMap)throws Exception{
-//        String examId = request.getParameter("examId");
-//        Examination exam = (Examination) baseManager.getObject(Examination.class.getName(), examId);
-//
-//        modelMap.put("examination", exam);
-//        return new ModelAndView("/question/examinationResult", modelMap);
-//    }
+    @Autowired
+    private BaseManager baseManager;
+
+    @RequestMapping("/start.do")
+    public ModelAndView start(){
+        return new ModelAndView("/question/activityDescription");
+    }
+
+    @RequestMapping("/startHelp.do")
+    public ModelAndView startHelp(HttpServletRequest request, ModelMap modelMap)throws Exception{
+        String examId = request.getParameter("examId");
+        Examination exam = (Examination) baseManager.getObject(Examination.class.getName(), examId);
+
+        modelMap.put("examination", exam);
+
+        return new ModelAndView("/question/activityDescription");
+    }
+
+    @RequestMapping("/shareReturn.do")
+    public ModelAndView shareReturn(HttpServletRequest request, ModelMap modelMap)throws Exception{
+        String examId = request.getParameter("examId");
+        Examination exam = (Examination) baseManager.getObject(Examination.class.getName(), examId);
+
+        modelMap.put("examination", exam);
+        return new ModelAndView("/question/examinationResult", modelMap);
+    }
 
 
 
 
-//    @RequestMapping("/answerExaminationSave.do")
-//    public ModelAndView saveQuestionAnswer(HttpServletRequest request){
-//        String examId = request.getParameter("examId");
-//        String answerList = request.getParameter("answerList");
-//        String[] answers = answerList.split(",");
-//
-//        Examination exam = (Examination) baseManager.getObject(Examination.class.getName(), examId);
-//        List<ExaminationQuestion> eqList = exam.getExaminationQuestionList();
-//
-//        for (ExaminationQuestion eq: eqList){
-//            String answer = answers[eq.getQuestionOrder()-1];
-//            eq.setAnswer(answer);
-//            if (answer.equalsIgnoreCase(eq.getQuestion().getAnswerTrue())){
-//                eq.setAnswerStatus("1");
-//            }else {
-//                eq.setAnswerStatus("2");
-//            }
-//            baseManager.saveOrUpdate(eq.getClass().getName(), eq);
-//        }
-//
-//        return new ModelAndView("/question/examinationResult");
-//    }
+    @RequestMapping("/answerExaminationSave.do")
+    public ModelAndView saveQuestionAnswer(HttpServletRequest request){
+        String examId = request.getParameter("examId");
+        String answerList = request.getParameter("answerList");
+        String[] answers = answerList.split(",");
+
+        Examination exam = (Examination) baseManager.getObject(Examination.class.getName(), examId);
+        List<ExaminationQuestion> eqList = exam.getExaminationQuestionList();
+
+        for (ExaminationQuestion eq: eqList){
+            String answer = answers[eq.getQuestionOrder()-1];
+            eq.setAnswer(answer);
+            if (answer.equalsIgnoreCase(eq.getQuestion().getAnswerTrue())){
+                eq.setAnswerStatus("1");
+            }else {
+                eq.setAnswerStatus("2");
+            }
+            baseManager.saveOrUpdate(eq.getClass().getName(), eq);
+        }
+
+        return new ModelAndView("/question/examinationResult");
+    }
 
 }
