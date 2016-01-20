95f9fb21c5cc33f5ec70af73fc35c7ac2f15e9bb
lylylyly <360682644@qq.com>
2016-01-20 13:32:43 +0800

    答题

--- aq-website/src/main/java/com/efeiyi/wx/website/util/WxQAConst.java
+++ aq-website/src/main/java/com/efeiyi/wx/website/util/WxQAConst.java
@@ -7,6 +7,7 @@ public class WxQAConst {
 
     public static final String HOSTNAME = "www.efeiyi.com";
     public static final String APPID = "wx2db1e25ab3dac836";
+    public static final String APPSECRET = "a39099dfb48bb588c1fc7b0d538d3a2b";
     public static final String dataKey = "wxqaopenid";
 
 }
