ff955bda8486445be4733d814dbdf9b3dd7940ae
lylylyly <360682644@qq.com>
2016-01-27 16:14:15 +0800

    答题

--- aq-website/src/main/java/com/efeiyi/wx/website/interceptor/WxQAInterceptor.java
+++ aq-website/src/main/java/com/efeiyi/wx/website/interceptor/WxQAInterceptor.java
@@ -27,7 +27,7 @@ public class WxQAInterceptor extends HandlerInterceptorAdapter {
 //        System.out.println(new Date() + "openid:" + openid + " code:" + request.getParameter("code"));
         String requestPath = request.getServletPath();
         request.getSession().setAttribute("requestPath",requestPath);
-        System.out.println("intercept--openid:" + openid);
+        System.out.println(new Date() + "\nintercepted--openid:" + openid);
         if ((openid == null || "".equals(openid))) {
             response.sendRedirect("http://www.efeiyi.com/wx/getUserBaseInfo.do?dataKey=openid;nickname;headimgurl&callback=dati.efeiyi.com/answer/getUserInfo2.do&source=dati");
 //            response.sendRedirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + WxQAConst.APPID + "&redirect_uri=http://dati.efeiyi.com/answer/getUserInfo.do&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect");
